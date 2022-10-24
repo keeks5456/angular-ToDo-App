4bcf9a197b57b53ca82fe37e7b3fe299b7859fe9
Akiko Green <agreen@alphaworks.tech>
2022-10-24 15:59:03 -0700

    feat(add observable & services to tasks component)

--- angular-todo-app/src/app/components/tasks/tasks.component.ts
+++ angular-todo-app/src/app/components/tasks/tasks.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Task } from 'src/Task';
-import { TASKS } from 'src/mock-task';
-
+import { TaskService } from 'src/app/services/task.service';
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
@@ -9,10 +8,11 @@ import { TASKS } from 'src/mock-task';
 })
 export class TasksComponent implements OnInit {
 
-    tasks: Task[] = TASKS
-  constructor() { }
+    tasks: Task[] = []
+  constructor(private taskService: TaskService) { }
 
   ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks) => this.tasks = tasks)
   }
 
 }
