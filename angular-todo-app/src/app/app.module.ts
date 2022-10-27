040586cbc42def4418d604cf59070d879fc3844f
Akiko Green <agreen@alphaworks.tech>
2022-10-26 17:01:49 -0700

    feat(toggle add task button, need fixing)

--- angular-todo-app/src/app/app.module.ts
+++ angular-todo-app/src/app/app.module.ts
@@ -9,7 +9,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { TaskItemComponent } from './components/task-item/task-item.component';
-import { AddTaskComponent } from './component/add-task/add-task.component';
+import { AddTaskComponent } from './components/add-task/add-task.component';
 
 @NgModule({
   declarations: [
